aef4e6c46c1ce4b2ff2ca090fca70be892e7aa75
David <ldavidpace@gmail.com>
2020-04-24 02:31:52 -0600

    added picture by script

--- UploadFileExe/index.js
+++ UploadFileExe/index.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
 const ncp = require('ncp').ncp;
 const path = require('path');
 const readline = require('readline');
-var cmd = require('node-cmd');
+var {exec} = require("child_process");
+var genImageFile = require('..\\bin\\picGen');
 
 const askQuestion = (query) => {
   const rl = readline.createInterface({
@@ -13,35 +15,62 @@ return new Promise(resolve => rl.question(query, ans => {
     rl.close();
     resolve(ans);
   }));
-} 
+}
+
+var deleteFolderRecursive = async function(folder, leaveDir) {
+  console.log('checking for folder', folder);
+  
+  if (fs.existsSync(folder)) {
+    console.log('deleteing folder');
+    fs.readdirSync(folder).forEach(async (file, index) => {
+      const curPath = path.join(folder, file);
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        await deleteFolderRecursive(curPath);
+      } else { // delete file
+        await new Promise((resolve) => fs.unlink(curPath, () => resolve()))
+      }
+    });
+    if (!leaveDir) {
+      return new Promise( resolve => fs.rmdir(folder, () => resolve()));
+    }
+  }
+};
 
 module.exports = async (destinationPath) => {
   if (!destinationPath) {
     destinationPath = path.resolve(__dirname, '..');
   }
+  const imgeFolderDest = path.resolve(destinationPath, 'src/images');
   let folderOfImages = '';
   if (process.argv[2]) {
-    folderOfImages = process.argv[2];
+    folderOfImages = process.argv[2]
   } else {
-    const answer = await askQuestion("This only works if you drag and drop and image.");
+    folderOfImages = await askQuestion("This only works if you drag and drop and image.");
+  }
+  console.log(folderOfImages);
+  console.log(imgeFolderDest);
+  console.log(destinationPath);
+  try {
+
+    await deleteFolderRecursive(imgeFolderDest);
+
+    await new Promise((resolve) => ncp(process.argv[2], imgeFolderDest, (err) => {
+      if (err) { 
+        console.error(err);
+        return;
+      }
+      resolve();
+    }));
+  
+    genImageFile(imgeFolderDest);
+    
+    
+    const answer = await askQuestion("We are finished. Just press enter and wait for the window to close");
+    console.log(answer);
+  } catch(err) {
+    console.log(err);
+    const answer = await askQuestion("We have failed. Just press enter");
+    console.log(answer);
   }
 
-  await new Promise((resolve) => ncp(process.argv[2], path.resolve(destinationPath, 'src/images'), (err) => {
-    if (err) { 
-      console.error(err);
-      return;
-    }
-    resolve();
-  }));
-
-  const output = await new Promise((resolve) => cmd.get(`
-    cd ${destinationPath}
-    npm run images
-  `, (stdout) => {
-    resolve(stdout);
-  }));
-  console.log("Current Directory", __dirname);
-
-  const answer = await askQuestion("We are finished. Just press enter");
-  console.log(answer);
-}
+  }
--- UploadFileExe/makeBin.js
+++ UploadFileExe/makeBin.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-
 fs.writeFileSync(path.resolve(__dirname, 'dist/bin.js'), `
   const uploadFiles = require('..');
 
-  uploadFiles("${path.resolve(__dirname, '..')}");
+  uploadFiles("${path.resolve(__dirname, '..').replace(/\\/g, '\\\\')}");
 
 `);
