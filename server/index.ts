45bfb90d6ee3a8d8ca447c0598fcc50fc547296e
David Pace <ldavidpace@gmail.com>
2023-03-09 19:52:59 -0700

    Added better redirecting and fixed the dev command

--- server/index.ts
+++ server/index.ts
@@ -2,15 +2,17 @@ import * as express from 'express';
 import * as path from 'path';
 const app = express();
 
+app.use((request, response, next) => {
+    if (process.env.NODE_ENV != 'development' && request.headers['x-forwarded-proto'] !== 'https') {
+      return response.redirect("https://" + request.headers.host + request.url);
+   }
+   return next();
+});
+
 app.use(express.static(__dirname + '/../build'));
 
 
 app.get('*', (request, response) => {  
-  console.log(`Request for ${request.path} forwarded proto ${request.headers['x-forwarded-proto']}`)
-  if (process.env.NODE_ENV != 'development' && request.headers['x-forwarded-proto'] !== 'https') {
-    return response.redirect("https://" + request.headers.host + request.url);
- }
-
   response.sendFile(path.resolve(__dirname,'../build/index.html'));
 });
 
